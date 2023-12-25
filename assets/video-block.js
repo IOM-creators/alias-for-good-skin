61fda7f79283ca24d0950d41774d758517e9d89a
Nikolas <iom.creators.ua@gmail.com>
2023-12-25 02:36:11 +0200

    video block added

---
+++ assets/video-block.js
@@ -0,0 +1,36 @@
+window.onload = (event) => {
+    const htmlTag = document.querySelector('html');
+    const videoBlock = document.querySelectorAll('.video-block');
+
+    videoBlock.forEach(videoItem => {
+        const openPopupButtons = videoItem.querySelectorAll('.video-block__open span');
+        const popupBlock = videoItem.querySelector('.popup');
+        const popupBlockInner = popupBlock.querySelector('.popup__inner');
+        const closePopupButton = videoItem.querySelector('.popup__close');
+        const videoElement = videoItem.querySelector('.video-block__popup-video');
+        
+        openPopupButtons.forEach((button) => {
+          button.addEventListener('click', (e) => {
+            e.preventDefault();
+            htmlTag.classList.add('overflow');
+            popupBlock.classList.add('active');
+            videoElement.play();
+          })
+        });
+        
+        closePopupButton.addEventListener('click', () => {
+          htmlTag.classList.remove('overflow');
+          popupBlock.classList.remove('active');
+          videoElement.pause();
+        });
+        
+        document.addEventListener('click', (e) => {
+          if (e.target === popupBlockInner) {
+            htmlTag.classList.remove('overflow');
+            popupBlock.classList.remove('active');
+            videoElement.pause();
+          }
+        });
+
+    });
+};
