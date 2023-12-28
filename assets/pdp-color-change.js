297cb50163844133d145f973d7ddaa994bc0abfa
Oleh <oleg.statkevych@bigdropinc.com>
2023-12-28 16:35:01 +0200

    update pdp

--- assets/pdp-color-change.js
+++ assets/pdp-color-change.js
@@ -8,3 +8,20 @@ window.addEventListener('scroll', function () {
         element.style.opacity = opacity;
     });
 });
+
+
+const ingredientsAccordionItems = document.querySelectorAll('.ingredients-expand')
+ingredientsAccordionItems.forEach(item => {
+    const button = item.querySelector('.upperlink')
+    const body = item.querySelector('.ingredients-expand__body')
+    button.addEventListener('click', function () {
+        if (this.classList.contains('is-open')) {
+            body.style.height = '0'
+            button.classList.remove('is-open');
+        } else {
+            let childHeight = body.children[0].scrollHeight
+            body.style.height = `${childHeight}px`;
+            button.classList.add('is-open');
+        }
+    })
+})
