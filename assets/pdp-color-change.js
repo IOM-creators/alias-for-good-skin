5331746625333840d0f123b2592ee2418bd5ee88
Oleh <oleg.statkevych@bigdropinc.com>
2024-01-03 15:12:29 +0200

    update pdp

--- assets/pdp-color-change.js
+++ assets/pdp-color-change.js
@@ -1,13 +1,13 @@
-window.addEventListener('scroll', function () {
-    var scrollTop = window.scrollY;
+// window.addEventListener('scroll', function () {
+//     var scrollTop = window.scrollY;
 
-    var colorBgElements = document.querySelectorAll('.product__colorbg');
-    colorBgElements.forEach(function (element) {
-        var elementHeight = element.clientHeight;
-        var opacity = 1 - (elementHeight - scrollTop) / elementHeight;
-        element.style.opacity = opacity;
-    });
-});
+//     var colorBgElements = document.querySelectorAll('.product__colorbg');
+//     colorBgElements.forEach(function (element) {
+//         var elementHeight = element.clientHeight;
+//         var opacity = 1 - (elementHeight - scrollTop) / elementHeight;
+//         element.style.opacity = opacity;
+//     });
+// });
 
 
 const ingredientsAccordionItems = document.querySelectorAll('.ingredients-expand')
